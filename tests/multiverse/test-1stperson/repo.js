60a4f62ab3d5802789137e65390f75fdcc934154
Jan Hořava <czechinthemail@gmail.com>
2023-09-19 15:49:43 -0700

    removed unused keybind

--- tests/multiverse/test-1stperson/repo.js
+++ tests/multiverse/test-1stperson/repo.js
@@ -44,7 +44,6 @@ function repo_init(){
         },
       },
       'keybinds': {
-        70: {},
         192: {
           'todo': function(){
               webgl_characters[webgl_character_id]['automove'] = !webgl_characters[webgl_character_id]['automove'];
