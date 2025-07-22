5f9b8dc104f0c6328aac1c3df18bb25e8f932ae4
Jan Hořava <czechinthemail@gmail.com>
2025-07-22 01:06:51 -0700

    added instant start standalone webgl test

---
+++ tests/multiverse/test-instant/repo.js
@@ -0,0 +1,65 @@
+'use strict';
+
+function new_game(){
+    webgl_level_load({
+      'character': 0,
+      'json': {
+        'clear_color': [0, .2, 0],
+        'characters': [
+          {
+            'id': '_instant',
+            'spawn': false,
+            'entities': [
+              {
+                'id': 'base',
+                'texture': 'grid.png',
+                'texture_x': 10,
+                'texture_y': 10,
+                'vertex_colors': [
+                  .5, .5, .5, 1,
+                ],
+                'vertices': [
+                  50, 0, -50,
+                  -50, 0, -50,
+                  -50, 0, 50,
+                  50, 0, 50,
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    });
+    webgl_character_init({
+      'collide_bottom': 8,
+      'collide_top': 2,
+      'collides': true,
+      'controls': 'rpg',
+      'level': -1,
+      'spawn': {
+        'position_x': 0,
+        'position_y': 6,
+        'position_z': 0,
+      },
+    });
+}
+
+function repo_init(){
+    core_repo_init({
+      'pointerbinds': {
+        'contextmenu': {
+          'preventDefault': true,
+        },
+        'pointermove': {
+          'todo': function(){
+              webgl_controls_pointer();
+          },
+        },
+      },
+      'root': '../../webgl-standalone.htm',
+      'storage_controls': true,
+      'title': 'Docs.htm',
+    });
+
+    new_game();
+}
