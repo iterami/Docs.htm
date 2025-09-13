693ece87b735ebac5656246a784e64a0343c2cda
Jan Hořava <czechinthemail@gmail.com>
2025-09-13 02:17:01 -0700

    added picking debug setting

--- tests/multiverse/test-instant/repo.js
+++ tests/multiverse/test-instant/repo.js
@@ -1,5 +1,138 @@
 'use strict';
 
+function debug_pick_color(args){
+    const pixelarray = new Uint8Array(3);
+    webgl.readPixels(
+      args.x,
+      webgl.drawingBufferHeight - args.y,
+      1,
+      1,
+      webgl.RGB,
+      webgl.UNSIGNED_BYTE,
+      pixelarray
+    );
+    return {
+      'pixelarray': pixelarray,
+      'x': args.x,
+      'y': webgl.drawingBufferHeight - args.y,
+    };
+}
+
+function debug_pick_entity(cursor){
+    const x = webgl_properties.pointerlock ? Math.floor(globalThis.innerWidth / 2) : core_pointer.x;
+    const y = webgl_properties.pointerlock ? Math.floor(globalThis.innerHeight / 2) : core_pointer.y;
+
+    webgl_shader_use('picking');
+    const color = webgl_scissor({
+      'todo': function(){
+          webgl_draw_picking();
+          return webgl_pick_color({
+            'x': x,
+            'y': y,
+          });
+      },
+      'x': x,
+      'y': y
+    });
+
+    let picked = false;
+    if(color[0] !== 0
+      || color[1] !== 0
+      || color[2] !== 0){
+        const color_blue = color[2] === 0
+          ? 0
+          : core_round({
+              'decimals': 3,
+              'number': color[2] / 255,
+            });
+        const color_green = color[1] === 0
+          ? 0
+          : core_round({
+              'decimals': 3,
+              'number': color[1] / 255,
+            });
+        const color_red = color[0] === 0
+          ? 0
+          : core_round({
+              'decimals': 3,
+              'number': color[0] / 255,
+            });
+
+        for(const id in entity_entities){
+            const entity = entity_entities[id];
+
+            if(entity.picking
+              && color_blue === entity.picking[2]
+              && color_green === entity.picking[1]
+              && color_red === entity.picking[0]){
+                if(entity.picking_range > 0){
+                    const position = webgl_get_position(entity);
+                    const distance = math_distance({
+                      'x0': character.position_x,
+                      'y0': character.position_y,
+                      'z0': character.position_z,
+                      'x1': position.x,
+                      'y1': position.y,
+                      'z1': position.z,
+                    });
+                    if(distance > entity.picking_range){
+                        break;
+                    }
+                }
+
+                picked = entity;
+                break;
+            }
+        }
+    }
+
+    if(picked){
+        if(picked.picking_xyz){
+            const rgb = webgl_draw_picked({
+              'picked': picked,
+              'x': x,
+              'y': y,
+            });
+
+            const position = webgl_get_position(picked);
+            webgl_picked_x = position.x + (rgb[0] / 255 - .5) * (picked.vertices[0] - picked.vertices[3]);
+            webgl_picked_y = position.y + (rgb[1] / 255 - .5) * (picked.vertices[1] - picked.vertices[7]);
+            webgl_picked_z = position.z + (rgb[2] / 255 - .5) * (picked.vertices[8] - picked.vertices[2]);
+        }
+
+        if(cursor === true){
+            webgl.canvas.style.cursor = 'pointer';
+            if(core_elements.reticle){
+                core_elements.reticle.style.height = '6px';
+                core_elements.reticle.style.width = '6px';
+            }
+
+        }else{
+            webgl_event({
+              'parent': picked,
+              'target': webgl_characters[webgl_character_id],
+            });
+        }
+
+    }else if(cursor === true){
+        webgl.canvas.style.cursor = 'auto';
+        if(core_elements.reticle){
+            core_elements.reticle.style.height = '4px';
+            core_elements.reticle.style.width = '4px';
+        }
+    }
+
+    webgl_shader_use('default');
+    webgl_draw();
+
+    return {
+      'color': color,
+      'picked': picked,
+      'x': x,
+      'y': y,
+    };
+}
+
 function new_game(){
     webgl_level_load({
       'character': 0,
@@ -71,6 +204,7 @@ function new_game(){
 
 function repo_init(){
     core_repo_init({
+      'info': '<div id=debug_color></div><div id=debug_entity></div>',
       'pointerbinds': {
         'contextmenu': {
           'preventDefault': true,
@@ -86,31 +220,55 @@ function repo_init(){
       },
       'root': '../../webgl-standalone.htm',
       'storage': {
+        'debug_picking': false,
         'pointerlock': true,
       },
       'storage_controls': true,
-      'storage_menu': '<table><tr><td><input id=pointerlock type=checkbox><td>Pointerlock</table>',
+      'storage_menu': '<table><tr><td><input id=debug_picking type=checkbox><td>Debug Picking'
+        + '<tr><td><input id=pointerlock type=checkbox><td>Pointerlock</table>',
       'title': 'Docs.htm',
       'ui': '<span id=picked></span><div id=color></div>',
+      'ui_elements': [
+        'debug_result',
+      ],
     });
 
     new_game();
 }
 
 function repo_logic(){
+    let color = '';
+    let debug_color = '';
+    let debug_entity = '';
+    let picked = '';
     const x = webgl_properties.pointerlock ? Math.floor(globalThis.innerWidth / 2) : core_pointer.x;
     const y = webgl_properties.pointerlock ? Math.floor(globalThis.innerHeight / 2) : core_pointer.y;
 
     webgl_draw();
-    const color = webgl_pick_color({
-      'x': x,
-      'y': y,
-    });
-    const picked = webgl_pick_entity(true);
+    if(core_storage_data.debug_picking){
+        const pick_color = debug_pick_color({
+          'x': x,
+          'y': y,
+        });
+        color = pick_color.pixelarray;
+        const pick_entity = debug_pick_entity(true);
+        picked = pick_entity.picked;
+
+        debug_color = 'Color: x' + pick_color.x + ' y' + pick_color.y + ' ' + color;
+        debug_entity = 'Entity: x' + pick_entity.x + ' y' + pick_entity.y + ' ' + pick_entity.color;
+
+    }else{
+        color = webgl_pick_color({
+          'x': x,
+          'y': y,
+        });
+        picked = webgl_pick_entity(true);
+    }
     core_ui_update({
-      'class': true,
       'ids': {
         'color': color,
+        'debug_color': debug_color,
+        'debug_entity': debug_entity,
         'picked': picked
           ? picked.id
           : 'false',
