5da86e191c8e6c2c2ac3823003f7406e7746a8c4
Jan Hořava <czechinthemail@gmail.com>
2026-04-26 23:42:38 -0700

    registered service worker, log events, and added icons

--- tests/pwa/service_worker.js
+++ tests/pwa/service_worker.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function handle_activate(event){
+    console.log('activate', event);
+}
+
+function handle_fetch(event){
+    console.log('fetch', event);
+}
+
+function handle_install(event){
+    console.log('install', event);
+}
+
+self.addEventListener('activate', handle_activate);
+self.addEventListener('fetch', handle_fetch);
+self.addEventListener('install', handle_install);
