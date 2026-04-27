ccf8efc6741ebfd45fa321790ec55edaa3d2bca0
Jan Hořava <czechinthemail@gmail.com>
2026-04-27 01:12:54 -0700

    added event handling, updated manifest scope and display

--- tests/pwa/service_worker.js
+++ tests/pwa/service_worker.js
@@ -1,17 +1,40 @@
 'use strict';
 
 function handle_activate(event){
-    console.log('activate', event);
+    event.waitUntil(
+      caches.keys()
+        .then(keys => Promise.all(
+          keys.map(key => (key === id
+            ? Promise.resolve()
+            : caches.delete(key))
+        )
+        .then(() => self.clients.claim())
+    );
 }
 
 function handle_fetch(event){
-    console.log('fetch', event);
+    event.respondWith(
+      caches.match(event.request).then(cached => cached
+        || fetch(event.request).catch(() => {
+            if(event.request.mode === 'navigation'){
+                return caches.match('index.htm');
+            }
+        });
+      )
+    );
 }
 
 function handle_install(event){
-    console.log('install', event);
+    event.waitUntil(
+      caches.open(id)
+        .then(cache => cache.addAll(files))
+        .then(() => self.skipWaiting())
+    );
 }
 
 self.addEventListener('activate', handle_activate);
 self.addEventListener('fetch', handle_fetch);
 self.addEventListener('install', handle_install);
+
+const id = 'index.htm?v1';
+const files = ['.', 'index.htm', 'manifest.json', '192.png', '512.png'];
